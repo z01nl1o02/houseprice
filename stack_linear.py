9cc6e092cfb1d6ab473839a40381b49d025a194a
ss <ss@01>
2017-07-14 18:43:25 +0800

    update linear stack

--- stack_linear.py
+++ stack_linear.py
@@ -19,33 +19,43 @@ class STACK_LINEAR:
         self._name = "stacking.linear"
         self._outdir = outdir
         self._clfs = []
-        self._clf2 = Ridge(alpha = 1.0, random_state=4000, normalize=False)
+        self._clf2 = Ridge(alpha = 10, random_state=4000, normalize=False)
     def add_clf(self,clf):
         self._clfs.append(clf)
         return
     def name(self):
         return self._name
+    def split(self,X,Y):
+        idx = X.index.tolist()
+        L = []
+        for k in range(0,len(idx),2):
+            L.append(idx[k])
+        X0 = X.loc[L,:]
+        Y0 = Y.loc[L]
+        for k in range(1,len(idx),2):
+            L.append(idx[k])
+        X1 = X.loc[L,:]
+        Y1 = Y.loc[L]
+        return (X0,Y0,X1,Y1)
     def train_one_clf(self,clf, trainX, trainY):
         clf.train(trainX, trainY)
-        clf.write(self._outdir)
+        #clf.write(self._outdir)
         return
-    def train(self,trainX,trainY):
-        trainC = np.empty( (len(self._clfs), len(trainY) ) )
+    def train(self,trainX,trainY,savemodel=0):
+        X0,Y0,X1,Y1 = self.split(trainX,trainY)
+        trainC = np.empty( (len(self._clfs), len(Y1) ) )
         for k,clf in enumerate(self._clfs):
-            self.train_one_clf(clf,trainX, trainY)
-            trainC[k,:] = np.asarray( clf.predict(trainX) )
+            self.train_one_clf(clf,X0, Y0)
+            trainC[k,:] = np.asarray( clf.predict(X1) )
         trainC = np.transpose(trainC)
-        self._clf2.fit(trainC, trainY)
+        self._clf2.fit(trainC, Y1)
         return
     def predict_one_clf(self,clf,testX):
         name = clf.name()
-        clf.read(self._outdir)
         testC = clf.predict(testX)
         testC = np.expm1(testC)
-        pd.DataFrame({'Id':testX['Id'],'SalePrice':testC}).to_csv(os.path.join(self._outdir,name + '.csv'), 
-                index=False,columns='Id,SalePrice'.split(','))
         return testC
-    def predict(self, testX):
+    def predict(self, testX,readmodel=0):
         testC = np.empty( (len(self._clfs), testX.shape[0] ) )
         for k,clf in enumerate(self._clfs):
             testC[k,:] = np.asarray( self.predict_one_clf(clf,testX)  )
